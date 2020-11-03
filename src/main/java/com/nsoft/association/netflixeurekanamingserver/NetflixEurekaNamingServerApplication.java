5cb53913d10dd80e0875aa2c4208dd4d341f5366
w2cluster575 <w2cluster575@w2cluster628.iiht.tech>
2020-11-03 16:18:21 +0530

    server

--- src/main/java/com/nsoft/association/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ src/main/java/com/nsoft/association/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -7,9 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class NetflixEurekaNamingServerApplication {
-
+//eureka server
 	public static void main(String[] args) {
 		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
 	}
 
+	
 }
